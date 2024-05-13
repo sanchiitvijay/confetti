e0d694cb4c6513136582e0c02c13e915d0510ae4
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-05-14 01:15:07 +0530

    added like controller

---
+++ server/controllers/Like.js
@@ -0,0 +1,69 @@
+const Like = require("../models/Like")
+const Post = require("../models/Post")
+
+exports.getLikes = async(req, res)=> {
+    try {
+        const postId = req.body.postId;
+    
+        const likes = await Like.find({author: postId})
+    
+        return res.status(200).json({
+            success: true,
+            message: "likes has been fetched",
+            likes
+        })
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: "error while fetching likes"
+        })
+    }
+}
+
+exports.liked = async(req, res) => {
+    const authorId = req.user.id;
+    const postId = req.body;
+
+    const like = await Like.find({author: authorId})
+
+    if(like) {
+        const updatedPost = await Post.findByIdAndUpdate(
+            postId,
+            { $pull: { likes: like._id } },
+            { new: true }
+        )
+        
+        const deletedLike = await Like.findByIdAndDelete({_id:like._id});
+
+        if(!updatedPost || !deletedLike) {
+            return res.status(400).json({
+                success: false,
+                message: "Error while removing the like"
+            })
+        }
+    } else {
+        const newLike = await Like.create({
+            author: authorId,
+            post: postId,
+        })
+        const updatedPost = await Post.findByIdAndUpdate(
+            postId,
+            {$push: {likes: newLike._id}},
+            {new: true}
+        )
+
+        if(!newLike || !updatedPost) {
+            return res.status(400).json({
+                success: false,
+                message: "Error while adding the like"
+            })
+        }
+        like = newLike
+    }
+    return res.status(200).json({
+        success: true,
+        message: "Like unlike has been added",
+        like
+    })
+}
+
