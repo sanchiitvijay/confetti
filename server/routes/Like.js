2167ed93e8e1bc931955ea51f770ce880a3b82d4
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-05-15 01:55:04 +0530

    added routers, edit-post, and fixed some controllers function

---
+++ server/routes/Like.js
@@ -0,0 +1,10 @@
+const { Router } = require("express")
+const { auth } = require("../middlewares/auth")
+const { getLikes, liked } = require("../controllers/Like")
+
+
+const router = Router();
+
+
+router.route("liked").post(auth, liked);
+router.route("get-all-likes").get(auth, getLikes);
