66c47fa5669e1ae6135534e52422fddc5d10853c
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-05-17 13:17:59 +0530

    added deleteGradutes promoteStudent and added in router as well

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const { auth, isStudent, isAdmin} = require("../middlewares/auth")
 const { sendotp, login, signup, changePassword } = require("../controllers/Auth")
-const { getAllUsers, removeUser, editUser } = require("../controllers/User")
+const { getAllUsers, removeUser, editUser, deleteGraduates, promoteStudents } = require("../controllers/User")
 
 const router = Router();
 
@@ -11,3 +11,5 @@ router.route("/change-password").post(auth, changePassword)
 router.route("/get-all-user").get(auth, isAdmin, getAllUsers);
 router.route("/remove-user").delete(auth, isAdmin, removeUser);
 router.route("/edit-user").patch(auth, editUser)
+router.route("delete-graduates").delete(auth, isAdmin, deleteGraduates)
+router.route("promote-students").post(auth, isAdmin, promoteStudents)
