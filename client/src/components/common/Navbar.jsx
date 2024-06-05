09c1915a8675e97dfc9611a5c2ad195a0bb2ce4d
ashutosh <ak956901@gmail.com>
2024-06-05 16:30:48 +0530

    navbar

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,12 +1,21 @@
 import { Button } from '@mui/material'
 import React from 'react'
-
+import logo from "../../assets/confetti.png"
 const Nav = () => {
   return (
     
      <div >
-      <Button  >Ashu </Button>
+      <div className='w-[100px] h-[100px]'>
 
+      <img src={logo} className='w-fit object-cover ' alt="" />
+        
+        
+      </div>  
+    
+      <div>
+
+
+      </div>
      </div>
         
 
