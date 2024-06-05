a1248feed7a86d121fbfaf348ec743e1fea4280b
ashutosh <ak956901@gmail.com>
2024-06-05 20:36:09 +0530

    Fixed login and signup routin

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 const Button = ({text,nav}) => {
   const navigate=useNavigate();
   return (
-    <button onClick={()=>navigate({nav})} className="p-[3px] relative">
+    <button onClick={()=>navigate(nav)} className="p-[3px] relative">
         <div className="absolute inset-0 bg-gradient-to-r from-confettiYellow2 to-orange-400 rounded-lg" />
         <div className="px-8 py-2  bg-white rounded-[6px]  relative group transition hover:text-white duration-200 text-orange-400 hover:bg-transparent">
             {text}
