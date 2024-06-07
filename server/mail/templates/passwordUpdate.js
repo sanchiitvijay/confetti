752e371a30cec910dfd041ddf9b5b329e2434f66
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-06-07 11:54:38 +0530

    added useForm and fix some issues

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -55,7 +55,7 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
+            <a hregister="https://studynotion-edtech-project.vercel.app"><img class="logo"
                     src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
@@ -66,7 +66,7 @@ exports.passwordUpdated = (email, name) => {
             </div>
             <div class="support">If you have any questions or need further assistance, please feel free to reach out to us
                 at
-                <a href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!
+                <a hregister="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!
             </div>
         </div>
     </body>
