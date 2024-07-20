3735ad0e076c74b8ccd6ff596e1825e8409378c2
sanchiitvijay <sanchiitvijay@gmail.com>
2024-07-21 01:18:45 +0530

    added transitions

--- client/src/components/common/PasswordInput.jsx
+++ client/src/components/common/PasswordInput.jsx
@@ -18,8 +18,6 @@ const PasswordInput = (props) => {
           {...props?.register(props?.value,{required:props?.required})}
           className="w-full bg-transparent border-transparent focus:border-transparent focus:ring-0 placeholder-white rounded-[0.5rem]  p-[12px]"
         />
-         {props?.error && <p className="text-red mt-1">{props?.value.charAt(0).toUpperCase() + props?.value.slice(1)} is required
-         <sup className="text-yellow-400">{" "}*</sup></p>}
         <div className='h-[1px] w-full bg-white'></div>
         <span
           onClick={() => setShowPassword((prev) => !prev)}
@@ -31,6 +29,8 @@ const PasswordInput = (props) => {
             <AiOutlineEye fontSize={24} fill="#FFFFFF" />
           )}
         </span>
+          {props?.error && <p className="ml-2 text-yellow-100 mt-1">{props?.value.charAt(0).toUpperCase() + props?.value.slice(1)} is required
+          <sup className="text-yellow-400">{" "}*</sup></p>}
     </label>
 
   )
