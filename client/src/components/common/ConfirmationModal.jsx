0a974bccda0f434a1b08127e841f7f58761f31c9
ashutosh <ak956901@gmail.com>
2024-07-28 21:16:29 +0530

    sidebar and outlet added with links

---
+++ client/src/components/common/ConfirmationModal.jsx
@@ -0,0 +1,31 @@
+import IconBtn from "../common/SubmitButton"
+
+export default function ConfirmationModal({ modalData }) {
+  return (
+    <div className="fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm">
+      <div className="w-11/12 max-w-[350px] rounded-lg border  p-6">
+        <p className="text-2xl font-semibold text-richblack-5">
+          {modalData?.text1}
+        </p>
+        <p className="mt-3 mb-5 leading-6 ">
+          {modalData?.text2}
+        </p>
+        <div className="flex items-center gap-x-4">
+          <IconBtn
+          customClasses="bg-yellow-100 text-black rounded-md px-4 py-2 font-semibold "
+            onclick={modalData?.btn1Handler}
+            disabled={false}
+            text={modalData?.btn1Text}
+
+          />
+          <button
+            className="cursor-pointer rounded-md  py-[8px] px-[20px] font-semibold "
+            onClick={modalData?.btn2Handler}
+          >
+            {modalData?.btn2Text}
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ client/src/components/core/Feed/dashboard-links.js
@@ -0,0 +1,20 @@
+export const sidebarLinks = [
+  {
+    id: 1,
+    name: "Feed",
+    path: "/feed/",
+    icon: "VscDashboard",
+  },
+  {
+    id: 2,
+    name: "My Profile",
+    path: "/feed/my-profile",
+    icon: "VscAccount",
+  },
+  {
+    id: 3,
+    name: "My Posts",
+    path:"/feed/my-posts",
+    icon:"VscPreview"
+  }
+];
