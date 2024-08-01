c8f8f03de679932cf79ce5f1b9dba2549ccd2e2e
sanchiitvijay <sanchiitvijay@gmail.com>
2024-08-02 01:04:04 +0530

    added createPost form, added commentForm

---
+++ client/src/components/common/CreatePost.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import { setPost } from '../../slices/postSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { createPost } from '../../services/operations/postAPI'
+
+const CreatePost = () => {
+    const [openMoreInfo, setOpenMoreInfo] = useState(false)
+    const [post, setPost] = useState({
+        description: "",
+        name: "",
+        year: "",
+        branch: "",
+    })
+    const dispatch = useDispatch()
+    const token = useSelector((state) => state.auth.token)
+    const {
+        description,
+        name,
+        year,
+        branch,
+    } = post
+    const handlePost = (e) => {
+        e.preventDefault()
+        setOpenMoreInfo(false)
+        dispatch(createPost(token, post))
+    }
+
+
+    const handleOnChange = (e) => {
+        setPost((prevData) => ({
+          ...prevData,
+          [e.target.name]: e.target.value,
+        }))
+      }
+
+  return (
+    <div>
+        <div className='border dark:text-white  md:w-[500px] bg-confettiLightColor2 dark:bg-confettiDarkColor3 border-black rounded-md m-3 md:m-12 p-3 md:p-4 pb-3'>
+            <label htmlFor="post" className="text-sm px-2" >Write Confession</label>
+            <input type="text" placeholder="What is in your mind" name="description"  onClick={() => !openMoreInfo && setOpenMoreInfo(true)}  onChange={handleOnChange} className="border border-black dark:border-white my-2 rounded-md p-2 h-12 w-full"/>
+
+            {
+                openMoreInfo && <> 
+                <label htmlFor="post" className="text-sm px-2">Name</label>
+            <input type="text" onChange={handleOnChange} placeholder="Do you know their name?" name="name" className="border border-black dark:border-white  my-2  text-xs md:text-sm rounded-md p-2 h-8 w-full"/>
+
+            <div className='flex flex-row max-md:gap-3 justify-between '>
+                <div className='flex flex-col'>
+                    <label htmlFor="post" className="text-sm pl-2">Year</label>
+                    <input type="text" onChange={handleOnChange} placeholder="do you know their year?" name="year" className="border border-black my-2 dark:border-white text-xs md:text-sm rounded-md h-8 w-max"/>
+                </div>
+                <div className='flex flex-col'>
+                    <label htmlFor="post" className="text-sm pl-2">Branch</label>
+                    <input type="text" onChange={handleOnChange} placeholder="do you know their branch?" name="branch" className="border border-black my-2 dark:border-white  text-xs md:text-sm rounded-md h-8 w-full"/>
+                </div>
+            </div>
+            <div className='mr-1 flex flex-row-reverse'>
+                <button className='dark:bg-confettiDarkColor4 border border-black dark:border-white text-black dark:text-white  bg-confettiLightColor4 rounded-md px-3 py-1 my-2' onClick={handlePost}>Post</button>
+            </div>
+            </>
+            }
+        </div>
+    </div>
+  )
+}
+
+export default CreatePost
--- client/src/components/core/Feed/Post.jsx
+++ client/src/components/core/Feed/Post.jsx
@@ -1,31 +1,30 @@
 import React, { useState } from 'react'
 import logo from "../../../assets/confettiNoText.png"
-import { VscGripper, VscKebabVertical } from "react-icons/vsc";
+import { VscSend } from "react-icons/vsc";
 import { IoShareSocialOutline, IoChatbubbleOutline } from "react-icons/io5";
 import { IoMdHeartEmpty } from "react-icons/io";
 import { useDispatch, useSelector } from 'react-redux';
 import { liked } from '../../../services/operations/likeAPI';
 import { reportPost } from '../../../services/operations/postAPI';
-import { getAllComments } from '../../../services/operations/commentAPI';
-import { Dropdown } from 'flowbite-react';
+import { createComments, getAllComments } from '../../../services/operations/commentAPI';
 import { FaHeart } from "react-icons/fa";
 import { setTotalLikes } from '../../../slices/postSlice';
-
-
-
-
+import Comment from '../../common/Comment';
+import PostHeader from '../../common/PostHeader';
 
 
 
 
 const Post = () => {
   const dispatch = useDispatch();
-  const [comment, setComment] = useState(false);
+  const [comments, setComments] = useState(false);
   // const likes = useSelector((state) => state.post.totalLikes);
   const token = useSelector((state) => state.auth.token);
   const post = useSelector((state) => state.post);
   const profile = useSelector((state) => state.profile);
   const [like, setLike] = useState(false);
+
+  const [comment, setComment] = useState("");
 
   if (post?.likes?.includes(profile?.user?._id)) {
     setLike(true);
@@ -46,67 +45,26 @@ const Post = () => {
   }
 
   const commentHandler = () => {
-    setComment(!comment);
+    setComments(!comments);
     dispatch(getAllComments(token, post));
   }
 
-  const reportHandler = () => {
-    dispatch(reportPost(token, post));
+  const handleSubmitComment = () => {
+    console.log("handle submit comment------------------", comment); 
+    dispatch(createComments(token, { post: post?._id, comment }));
   }
 
+
   return (
-    <div className='border dark:text-white w-[500px] bg-confettiLightColor2 dark:bg-confettiDarkColor3 border-black rounded-md m-12 p-4 pb-3'>
+    <div className='border dark:text-white  md:w-[500px] bg-confettiLightColor2 dark:bg-confettiDarkColor3 border-black rounded-md m-3 md:m-12 p-3 md:p-4 pb-3'>
       {/* post header */}
-      <div className='flex flex-row border-b border-black dark:border-white pb-3 justify-between'>
-        <div className='flex flex-row gap-3'>
-          <img
-            src={logo}
-            className='w-[40px] h-[40px] object-fill rounded-full border border-confettiDarkColor1 dark:border-confettiLightColor1'
-            alt='img'
-          />
-          <div className='flex flex-col'>
-            <h1 className='font-semibold text-sm'>username</h1>
-            <p className='text-xs'>college</p>
-          </div>
-        </div>
+        <PostHeader/>
 
-        <Dropdown
-          className='dark:bg-confettiDarkColor1 rounded-md'
-          arrowIcon={false}
-          inline={true}
-          label={
-            <button
-              type='button'
-              className='flex text-md rounded-full md:me-0'
-            >
-              <span className='sr-only'>Open user menu</span>
-              <VscGripper
-                className='my-auto'
-                fontSize={'30px'}
-              />
-            </button>
-          }
-        >
-
-          <div className='px-5 py-2 flex flex-col rounded-md mx-1 bg-confettiLightColor3 text-black dark:text-white dark:bg-confettiDarkColor3'>
-            <div className='flex flex-row justify-center '>
-              <div className='block cursor-pointer text-sm hover:underline' onClick={reportHandler}>
-                Report
-              </div>
-            </div>
-          </div>
-        </Dropdown>
-
-      </div>
-
-
-  
       {/* content */}
-      <div className='p-4 min-h-[200px] text-center text-xl content-center border-black dark:border-white border-b'>
+      <div className='p-3 md:p-4 min-h-[200px] text-center text-lg md:text-xl content-center border-black dark:border-white border-b'>
         Remember, contributions to this repository should follow our GitHub Community Guidelines. Trying to avoid encountering the strangest and most random errors while compiling the code. Trying to avoid encountering the strangest and most random errors while compiling the code. Trying to avoid encountering the strangest and most random errors while compiling the code.
       </div>
       
-
 
       {/* footer */}
       <div className='flex pt-2 justify-between px-5 flex-row'>
@@ -125,17 +83,13 @@ const Post = () => {
 
       {/* comments */}
       {
-        comment &&
-        <div className='p-4 border-t border-black mt-2'>
-        <div className='flex flex-row justify-between py-2'>
-          <div className='flex flex-row gap-3'>
-          <img src={logo} alt="" className='w-[28px] border border-black rounded-full h-[28px] '/>
-          <div className='text-[10px] my-auto'><span className='text-xs font-semibold mr-2'> Username: </span>Comment</div>
-          </div>
-          <VscKebabVertical fontSize={'20px'}  className='my-auto mr-1'/>
-      </div>
-        
-       
+        comments &&
+        <div className='max-md:px-2 p-4 border-t border-black mt-2'>
+          <div className='flex flex-row gap-5 pb-4 pt-2 px-1'>
+              <input type='text' placeholder='Add a comment' onChange={(e)=>setComment(e.target.value)} className='w-full h-9 border border-black rounded-md p-2 focus:ring-0 focus:outline-none  focus:border-black focus:shadow-lg'/>
+              <VscSend fontSize={30} className='my-auto' onClick={handleSubmitComment}/>
+            </div>
+          <Comment/>
       </div>
       }
     </div>
