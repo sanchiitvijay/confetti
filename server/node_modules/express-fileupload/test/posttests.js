3b2382ec2db89bf955e6fe607d31360dec3da234
sanchiitvijay <sanchiitvijay@gmail.com>
2024-08-03 22:46:45 +0530

    removed nodemodules hopefully

--- server/node_modules/express-fileupload/test/posttests.js
+++
@@ -1,5 +0,0 @@
-const { clearFileDir } = require('./server');
-
-console.log('clearing test files...');
-clearFileDir();
-console.log('done');
--- server/node_modules/express-fileupload/test/pretests.js
+++
@@ -1,5 +0,0 @@
-const { createTestFiles } = require('./server');
-
-console.log('creating test files...');
-createTestFiles()
-  .then(() => console.log('done'));
