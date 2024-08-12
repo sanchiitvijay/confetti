8ba7c3550795e9f642f0d22b5f1459274074bdcd
sanchiitvijay <sanchiitvijay@gmail.com>
2024-08-12 11:53:12 +0530

    leaderboard badge done, compressed the file

--- client/src/pages/StatsPage.jsx
+++ client/src/pages/StatsPage.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./../components/core/Feed/Settings/Settings.css"
 import { useSelector } from 'react-redux';
+import gold from './../assets/gold.png'
+import silver from './../assets/silver.png'
+import bronze from './../assets/bronze.png' 
+import "../components/core/Feed/MyProfile.css"
+
 
 const Stats = () => {
 
@@ -9,7 +14,7 @@ const Stats = () => {
 
   return (
     <div className='text-white text-center w-[90%]'>
-        <p className='text-2xl m-7'>Leaderboard</p>
+        <p className='text-2xl m-7 animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent font-black '>Leaderboard</p>
         <div className='flex md:flex-row flex-col gap-5 items-center justify-evenly'>
 
             {/* 1st component */}
@@ -18,14 +23,27 @@ const Stats = () => {
                     <p className='text-lg mb-5'>Top liked posts</p>
 
                     {/* map isme lgana hai */}
-                    {topLikes?.map((post) => (
-                    <div className='flex place-content-center flex-row gap-4 mt-5'>
-                        <img src={post?.displayPicture} alt={post?.username} className='w-[50px] h-[50px] rounded-full border-white border'/>
+                    {topLikes?.map((post, index) => (
+                    <div className='flex place-content-center gap-2 flex-row mt-5' key={index}>
+                    {
+                        index === 0 ? (
+                            <img src={gold} alt='gold' className='w-[18px] h-[18px]'/>
+                        ) : index === 1 ? (
+                            <img src={silver} alt='silver' className='w-[18px] h-[18px]'/>
+                        ) : index === 2 ? (
+                            <img src={bronze} alt='bronze' className='w-[18px] h-[18px] '/>
+                        ) : (
+                            <p>{index + 1}</p>
+                        )
+                    }
+                    <div className='gap-4 flex flex-row'>
+                        <img src={post?.displayPicture} alt={post?.username} className='w-[50px] h-[50px] rounded-full border-black dark:border-white border'/>
                         <div className='text-left'>
-                        <p className='my-auto'>{post?.username}</p>
-                        <p className='my-auto'>Likes Count: {post?.likes?.length}</p>
+                            <p className='my-auto'>{post?.username}</p>
+                            <p className='my-auto'>Likes Count: {post?.likes?.length}</p>
                         </div>
                     </div>
+                </div>
                     ))}
                 </div>
             </div>
@@ -37,12 +55,25 @@ const Stats = () => {
 
                     {/* map isme lagana hai */}
                     {
-                        topPost?.map((post) => (
-                            <div className='flex place-content-center flex-row gap-4 mt-5'>
-                                <img src={post?.displayPicture} alt={post?.username} className='w-[50px] h-[50px] rounded-full border-white border'/>
-                                <div className='text-left'>
-                                <p className='my-auto'>{post?.username}</p>
-                                <p className='my-auto'>Post Count: {post?.posts?.length}</p>
+                        topPost?.map((post, index) => (
+                            <div className='flex place-content-center gap-2 flex-row mt-5' key={index}>
+                                {
+                                    index === 0 ? (
+                                        <img src={gold} alt='gold' className='w-[18px] h-[18px]'/>
+                                    ) : index === 1 ? (
+                                        <img src={silver} alt='silver' className='w-[18px] h-[18px]'/>
+                                    ) : index === 2 ? (
+                                        <img src={bronze} alt='bronze' className='w-[18px] h-[18px] '/>
+                                    ) : (
+                                        <p>{index + 1}</p>
+                                    )
+                                }
+                                <div className='gap-4 flex flex-row'>
+                                    <img src={post?.displayPicture} alt={post?.username} className='w-[50px] h-[50px] rounded-full border-black dark:border-white border'/>
+                                    <div className='text-left'>
+                                        <p className='my-auto'>{post?.username}</p>
+                                        <p className='my-auto'>Likes Count: {post?.likes?.length}</p>
+                                    </div>
                                 </div>
                             </div>
                         ))
