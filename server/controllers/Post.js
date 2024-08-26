8141f48260785eed7764a3999c4ab7f30c8f2c69
sanchiitvijay <sanchiitvijay@gmail.com>
2024-08-27 02:45:16 +0530

    leaderboard, reply controller

--- server/controllers/Post.js
+++ server/controllers/Post.js
@@ -182,7 +182,6 @@ exports.createPost = async (req, res) => {
             })
            
             Promise.all(probableUserIds?.map(async(userId)=>{
-                    if(userId.toString()!=updatedUser._id.toString()) return null;
                     const notfRef=db.collection("Notifications").doc(userId.toString()).collection("notifications");
                     console.log("MAI HU USER",notfRef);
                     return notfRef.add({
