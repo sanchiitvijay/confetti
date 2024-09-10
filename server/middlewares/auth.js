8d8fac4707f449be5602b1f020df3d38c6895710
Ashutosh Kumar <123724919+ash956901@users.noreply.github.com>
2024-09-11 00:55:49 +0530

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -18,6 +18,7 @@ exports.auth=async(req,res,next)=>{
                 message:"Token is missing"
             })
         }
+        console.log("JWT SECRET:",process.env.JWT_SECRET);
         try{
             const decode=jwt.verify(token,process.env.JWT_SECRET);
             req.user=decode;
