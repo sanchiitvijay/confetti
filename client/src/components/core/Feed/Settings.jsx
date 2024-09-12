2d6f42cba49c767197e8580de0d8bea18eb62753
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-09-12 19:15:23 +0530

    suspense in settings.jsx

--- client/src/components/core/Feed/Settings.jsx
+++ client/src/components/core/Feed/Settings.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ChangeDp from './Settings/ChangeDp'
 import EditProfile from "../Feed/Settings/EditProfile"
 import ChangePassword from "../Feed/Settings/ChangePassword"
 import DeleteAccount from "../Feed/Settings/DeleteAccount"
+import Loader from "../../common/Loader"
+
+
 const Settings = () => {
   return (
-    <div className='dark:text-white  flex flex-col items-center gap-4 '>   
-        <ChangeDp/>
-        <EditProfile/>
-        <ChangePassword/>
-        <DeleteAccount/>
-        <div className='invisible'>dasflkasd;lfk;lasdkf</div>
-    </div>
+    <Suspense fallback={<Loader/>}>
+      <div className='dark:text-white  flex flex-col items-center gap-4 '>   
+          <ChangeDp/>
+          <EditProfile/>
+          <ChangePassword/>
+          <DeleteAccount/>
+          <div className='invisible'>dasflkasd;lfk;lasdkf</div>
+      </div>
+    </Suspense>
   )
 }
 
